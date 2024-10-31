c8d50b881e6333063ee54079a6a544c57e740f34
durandarthur <durandarthur09@outlook.fr>
2024-10-31 21:44:09 +0100

    particle test 10

--- src/App.vue
+++ src/App.vue
@@ -37,27 +37,24 @@ function handleMouseMove(event) {
 
 onMounted(() => {
 	// alert("en travaux !");
+	let particle;
+	for (let i = 1; i <= particlesAmount - 1; i++) {
+		particle = particles.value[i].mesh;
+
+		console.log(particle);
+
+		particle.position.x = Math.floor(document.body.clientWidth * Math.random());
+		particle.position.y = Math.floor(
+			document.body.clientHeight * Math.random()
+		);
+		particle.position.z = Math.floor(Math.random() * 100);
+
+		particle.rotation.x = Math.floor(360 * Math.random());
+		particle.rotation.y = Math.floor(360 * Math.random());
+		particle.rotation.z = Math.floor(360 * Math.random());
+	}
 	renderer?.value?.onBeforeRender(() => {
 		document.onmousemove = handleMouseMove;
-
-		let particle;
-		for (let i = 1; i <= particlesAmount - 1; i++) {
-			particle = particles.value[i].mesh;
-
-			console.log(particle);
-
-			// particle.position.x = Math.floor(
-			// 	document.body.clientWidth * Math.random()
-			// );
-			// particle.position.y = Math.floor(
-			// 	document.body.clientHeight * Math.random()
-			// );
-			// particle.position.z = Math.floor(Math.random() * 100);
-
-			// particle.rotation.x = Math.floor(360 * Math.random());
-			// particle.rotation.y = Math.floor(360 * Math.random());
-			// particle.rotation.z = Math.floor(360 * Math.random());
-		}
 	});
 });
 
@@ -187,19 +184,7 @@ const particlesAmount = 100;
 					<BasicMaterial :props="{ wireframe: true }" color="#04D9FF" />
 				</Box>
 				<Group>
-					<Tetrahedron
-						v-for="i in particlesAmount"
-						:key="i"
-						ref="particles"
-						:position="{
-							x: Math.floor(document.body.clientWidth * Math.random()),
-							y: Math.floor(document.body.clientHeight * Math.random()),
-						}"
-						:rotation="{
-							x: Math.floor(360 * Math.random()),
-							y: Math.floor(360 * Math.random()),
-							z: Math.floor(360 * Math.random()),
-						}"
+					<Tetrahedron v-for="i in particlesAmount" :key="i" ref="particles"
 						><BasicMaterial :props="{ wireframe: true }" color="#04D9FF"
 					/></Tetrahedron>
 				</Group>
